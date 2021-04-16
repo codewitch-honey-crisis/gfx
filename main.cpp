843b40e68029be0d59fd4d7662438964eb67f2b8
codewitch-honey-crisis <gaztoast@gmail.com>
2021-04-16 08:25:51 -0700

    added bitmap support and improved pixel with Y'UV support

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #define HTCW_LITTLE_ENDIAN
 #include <stdio.h>
-#include "include/gfx_pixel.hpp"
+#include "include/gfx_bitmap.hpp"
 using namespace gfx;
+template <typename PixelType>
+void dump_channel_values(const PixelType& p) {
+    printf("channels = { 0x%X",(int)p.template channel<0>());
+    if(p.channels>1) {
+        printf(", 0x%X",(int)p.template channel_unchecked<1>());
+        if(p.channels>2) {
+            printf(", 0x%X",(int)p.template channel_unchecked<2>());
+            if(p.channels>3) {
+                printf(", 0x%X",(int)p.template channel_unchecked<3>());
+            }
+        }
+    }
+    printf(" }\r\n");
+}
+template <typename BitmapType>
+void dump_bitmap(const BitmapType& bmp) {
+    static const char *col_table = " .%#";
+    using gsc2 = pixel<channel_traits<channel_name::L,2>>;
+    for(int y = 0;y<bmp.dimensions().height;++y) {
+        for(int x = 0;x<bmp.dimensions().width;++x) {
+            const typename BitmapType::pixel_type px = bmp[point16(x,y)];
+            char sz[2];
+            sz[1]=0;
+            const auto px2 = px.template convert<gsc2>();
+            size_t i =px2.template channel<0>();
+            sz[0] = col_table[i];
+            printf("%s",sz);
+            
+        }
+        printf("\r\n");
+    }
+}
 int main() {
 
     using bgr888 = pixel<
@@ -23,106 +55,96 @@ int main() {
     using mono1 = pixel<
         channel_traits<channel_name::L,1>
     >;
+    using gsc2 = pixel<
+        channel_traits<channel_name::L,2>
+    >;
+    using gsc4 = pixel<
+        channel_traits<channel_name::L,4>
+    >;
     using rgb888 = pixel<
             channel_traits<channel_name::R,8>,
             channel_traits<channel_name::G,8>,
             channel_traits<channel_name::B,8>
-        >;
-    auto pixel = rgb888(rand()%256,rand()%256,rand()%256);
-    // see if the two pixel types have the same
-    // channel names in the same order
-    printf("rgb888::equals<rgb565> = %s\r\n",
-        rgb888::equals<rgb565>::value?"true":"false");
+    >;
+    using rgb666 = pixel<
+            channel_traits<channel_name::R,6>,
+            channel_traits<channel_name::G,6>,
+            channel_traits<channel_name::B,6>
+    >;
+    using rgb232 = pixel<
+            channel_traits<channel_name::R,2>,
+            channel_traits<channel_name::G,3>,
+            channel_traits<channel_name::B,2>
+    >;
+    using rgb333 = pixel<
+            channel_traits<channel_name::R,3>,
+            channel_traits<channel_name::G,3>,
+            channel_traits<channel_name::B,3>
+    >;
 
-    // same as above, different pixel types
-    printf("bgr888::equals<rgb565> = %s\r\n",
-        bgr888::equals<rgb565>::value?"true":"false");
+    using rgb101210 = pixel<
+            channel_traits<channel_name::R,10>,
+            channel_traits<channel_name::G,12>,
+            channel_traits<channel_name::B,10>
+    >;
+    /* Currently not working (bug)
+    using rgb212221 = pixel<
+            channel_traits<channel_name::R,21>,
+            channel_traits<channel_name::G,22>,
+            channel_traits<channel_name::B,21>
+    >;*/
 
-    // see if the two pixel types have the same
-    // channel names in any order
-    printf("bgr888::unordered_equals<rgb565> = %s\r\n",
-        bgr888::unordered_equals<rgb565>::value?"true":"false");
+    using yuv888 = pixel<
+            channel_traits<channel_name::Y,8>,
+            channel_traits<channel_name::U,8>,
+            channel_traits<channel_name::V,8>
+    >;
 
-    // check for an RGB color model
-    printf("bgr888::has_channel_names<R,G,B> = %s\r\n",
-        bgr888::has_channel_names<
-            channel_name::R,
-            channel_name::G,
-            channel_name::B
-            >::value?"true":"false");
+    // set your pixel type to any one of the above
+    // or make your own pixel format     
+    using bmp_type = bitmap<gsc2>;
+    const size16 bmp_size(15,15);
 
-    printf("\r\n");
+    uint8_t buf[bmp_type::sizeof_buffer(bmp_size)];
 
-    // example checking for black and white
-    // color model
-    bool isBW = mono1::has_channel_names<
-            channel_name::L>::value &&
-            mono1::channels==1;
+    bmp_type bmp(bmp_size,buf);
 
-
-    printf("channel<0>::bits_to_left = %d\r\n",(int)mono1::channel_by_index<0>::bits_to_left);
-    printf("channel<0>::bits_to_right = %d\r\n",(int)mono1::channel_by_index<0>::bits_to_right);
-    printf("channel<0>::total_bits_to_right = %d\r\n",(int)mono1::channel_by_index<0>::total_bits_to_right);
-    printf("\r\n");
-    
-    mono1 m(1);
-    uint8_t mcb[sizeof(m)];
-    memcpy(mcb,&m,sizeof(mcb));
-    for(size_t i = 0;i<sizeof(mcb);++i) {
-        
-        printf("%02X",mcb[i]);
+    bmp.clear(bmp.bounds());
+    bool col = false;
+    for(int y=0;y<bmp.dimensions().height;++y) {
+        for(int x=0;x<bmp.dimensions().width;++x) {
+            if(x==0||y==0||x==bmp.bounds().right()||y==bmp.bounds().bottom()) {
+                bmp[point16(x,y)]=color<typename bmp_type::pixel_type>::white;
+            } else {
+                bmp[point16(x,y)]=col?
+                    color<typename bmp_type::pixel_type>::dark_blue:
+                    color<typename bmp_type::pixel_type>::purple;
+            }
+            col = !col;
+        }    
     }
-    printf("\r\n\r\n");
     
-    printf("channel<0>::bits_to_left = %d\r\n",(int)rgb888::channel_by_index<0>::bits_to_left);
-    printf("channel<0>::bits_to_right = %d\r\n",(int)rgb888::channel_by_index<0>::bits_to_right);
-    printf("channel<0>::total_bits_to_right = %d\r\n",(int)rgb888::channel_by_index<0>::total_bits_to_right);
-    printf("channel<1>::bits_to_left = %d\r\n",(int)rgb888::channel_by_index<1>::bits_to_left);
-    printf("channel<1>::bits_to_right = %d\r\n",(int)rgb888::channel_by_index<1>::bits_to_right);
-    printf("channel<1>::total_bits_to_right = %d\r\n",(int)rgb888::channel_by_index<1>::total_bits_to_right);
-    printf("channel<2>::bits_to_left = %d\r\n",(int)rgb888::channel_by_index<2>::bits_to_left);
-    printf("channel<2>::bits_to_right = %d\r\n",(int)rgb888::channel_by_index<2>::bits_to_right);
-    printf("channel<2>::total_bits_to_right = %d\r\n",(int)rgb888::channel_by_index<2>::total_bits_to_right);
-    printf("\r\n");
-    auto cvt=rgb888(true,1,0,.3333);
-    uint8_t cb[sizeof(cvt)];
-    auto cval = cvt.value();
-    memcpy(cb,&cval,sizeof(cb));
-    for(size_t i = 0;i<sizeof(cb);++i) {
-        
-        printf("%02X",cb[i]);
-    }
-    printf("\r\n\r\n");
-    printf("rgb565(true,1,0,.3333).convert<rgb888>().channel<0>() = %d\r\n",(int)cvt.channel<0>());
-    printf("rgb565(true,1,0,.3333).convert<rgb888>().channel<1>() = %d\r\n",(int)cvt.channel<1>());
-    printf("rgb565(true,1,0,.3333).convert<rgb888>().channel<2>() = %d\r\n",(int)cvt.channel<2>());
-    printf("\r\n");
-
-    rgb888 rgb=rgb888(true,1,0,.3333);
-    printf("rgb888(true,1,0,.3333).channel<0>() = %d\r\n",(int)rgb.channel<0>());
-    printf("rgb888(true,1,0,.3333).channel<1>() = %d\r\n",(int)rgb.channel<1>());
-    printf("rgb888(true,1,0,.3333).channel<2>() = %d\r\n",(int)rgb.channel<2>());
-    printf("\r\n");
-
-    printf("rgb888(true,1,0,.3333) memory layout (big endian): 0x");
-    uint8_t pb[sizeof(rgb)];
-    auto rval = rgb.value();
-    memcpy(pb,&rval,sizeof(pb));
-    for(size_t i = 0;i<sizeof(pb);++i) {
-        
-        printf("%02X",pb[i]);
-    }
-    printf("\r\n\r\n");
-
-    printf("color<gsc8>::red.channel<0>() = %d\r\n",(int)color<gsc8>::red.channel<0>());
-    printf("color<gsc8>::green.channel<0>() = %d\r\n",(int)color<gsc8>::green.channel<0>());
-    printf("color<gsc8>::blue.channel<0>() = %d\r\n",(int)color<gsc8>::blue.channel<0>());
-    printf("\r\n");
+    dump_bitmap(bmp);
+    rect16 r = bmp.bounds().inflate(-3,-3);
+    bmp.clear(r);
+    bmp.fill(r.inflate(-1,-1),color<bmp_type::pixel_type>::medium_aquamarine);
     
-    printf("color<mono1>::red.channel<0>() = %d\r\n",(int)color<mono1>::red.channel<0>());
-    printf("color<mono1>::green.channel<0>() = %d\r\n",(int)color<mono1>::green.channel<0>());
-    printf("color<mono1>::blue.channel<0>() = %d\r\n",(int)color<mono1>::blue.channel<0>());
-    printf("\r\n");
+    printf("\r\n\r\n");
+    dump_bitmap(bmp);
+    printf("\r\n\r\n");
+    const size16 bmp2_size(bmp_size.width*2,bmp_size.height*2);
+    uint8_t buf2[bmp_type::sizeof_buffer(bmp2_size)];
+    bmp_type bmp2(bmp2_size,buf2);
+    bmp2.clear(bmp2.bounds());
+    
+    bmp.blt(rect16(point16(8,8),size16(7,7)),bmp2,point16(0,0));
+    bmp.blt(rect16(point16(0,8),size16(7,7)),bmp2,point16(22,0));
+    bmp.blt(bmp.bounds(),bmp2,point16(7,7));
+    bmp.blt(rect16(point16(8,0),size16(7,7)),bmp2,point16(0,22));
+    bmp.blt(rect16(point16(0,0),size16(7,7)),bmp2,point16(22,22));
+    printf("bmp2:\r\n");
+    dump_bitmap(bmp2);
+        
     return 0;
 
 }

002d71e0cba82598a1e78f22622f9331b9e5301a
codewitch-honey-crisis <gaztoast@gmail.com>
2021-11-17 16:42:31 -0800

    fixed bug centering rectangles

--- src/gfx_positioning.hpp
+++ src/gfx_positioning.hpp
@@ -239,22 +239,25 @@ namespace gfx {
             y2+=y;
         }
         constexpr inline rectx center_horizontal(const rectx& bounds) const {
-            return offset((bounds.width()-width())/2,0);
+            return offset((bounds.width()-width())/2,0).offset(bounds.left(),bounds.top());
         }
         constexpr inline rectx center_vertical(const rectx& bounds) const {
-            return offset(0,(bounds.height()-height())/2);
+            return offset(0,(bounds.height()-height())/2).offset(bounds.left(),bounds.top());
         }
         constexpr inline rectx center(const rectx& bounds) const {
-            return offset((bounds.width()-width())/2,(bounds.height()-height())/2);
+            return offset((bounds.width()-width())/2,(bounds.height()-height())/2).offset(bounds.left(),bounds.top());
         }
         constexpr inline void center_horizontal_inplace(const rectx& bounds) {
             offset_inplace((bounds.width()-width())/2,0);
+            return offset_inplace(bounds.left(),bounds.top());
         }
         constexpr inline rectx center_vertical_inplace(const rectx& bounds) {
-            return offset_inplace(0,(bounds.height()-height())/2);
+            offset_inplace(0,(bounds.height()-height())/2);
+            return offset_inplace(bounds.left(),bounds.top());
         }
         constexpr inline rectx center_inplace(const rectx& bounds) {
-            return offset_inplace((bounds.width()-width())/2,(bounds.height()-height())/2);
+            offset_inplace((bounds.width()-width())/2,(bounds.height()-height())/2);
+            return offset_inplace(bounds.left(),bounds.top());
         }
         // normalizes a rectangle, such that x1<=x2 and y1<=y2
         constexpr inline rectx normalize() const {

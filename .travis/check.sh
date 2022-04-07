e7f97ee1956f0761422a4673751ddee7be7ac90c
honey the codewitch <gaztoast@gmail.com>
2022-04-07 07:41:20 -0700

    syntax error

--- .travis/check.sh
+++ .travis/check.sh
@@ -28,7 +28,7 @@ echo "Running pre-publish tests..."
 pio test -v
 TEST_RESULT=$?
 
-if [[ "${TEST_RESULT}" != "0"]]; then
+if [[ "${TEST_RESULT}" != "0" ]]; then
   echo "One or more tests failed. Aborting."
   exit 1
 fi

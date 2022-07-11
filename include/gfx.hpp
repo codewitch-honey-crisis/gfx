6ef0e3be3f64df8703cc17950337f2beced1d828
honey the codewitch <gaztoast@gmail.com>
2022-07-10 22:20:27 -0700

    fixed gfx_open_font not being directly included.

--- include/gfx.hpp
+++ include/gfx.hpp
@@ -7,6 +7,7 @@
 #include "gfx_positioning.hpp"
 #include "gfx_palette.hpp"
 #include "gfx_font.hpp"
+#include "gfx_open_font.hpp"
 #include "gfx_image.hpp"
 #include "gfx_drawing.hpp"
 #include "gfx_bitmap.hpp"

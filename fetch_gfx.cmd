94e578ed890c86298884c08bf69865a9d181ce7d
codewitch-honey-crisis <gaztoast@gmail.com>
2023-09-14 18:17:52 -0700

    added batch file for fetching deps

---
+++ fetch_gfx.cmd
@@ -0,0 +1,12 @@
+@rmdir htcw_bits /S /Q
+@git clone https://github.com/codewitch-honey-crisis/htcw_bits
+@rmdir htcw_data /S /Q
+@git clone https://github.com/codewitch-honey-crisis/htcw_data
+@rmdir htcw_io /S /Q
+@git clone https://github.com/codewitch-honey-crisis/htcw_io
+@rmdir htcw_ml /S /Q
+@git clone https://github.com/codewitch-honey-crisis/htcw_ml
+@rmdir htcw_gfx /S /Q
+@git clone https://github.com/codewitch-honey-crisis/gfx htcw_gfx
+
+

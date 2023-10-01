b6ac9d4e9f7147a49574cb1ba788b2b3235f930e
codewitch-honey-crisis <gaztoast@gmail.com>
2023-10-01 07:40:47 -0700

    updated fetch deps

--- fetch_deps.cmd
+++ fetch_deps.cmd
@@ -1,9 +1,5 @@
-@rmdir htcw_bits /S /Q
-@git clone https://github.com/codewitch-honey-crisis/htcw_bits
 @rmdir htcw_data /S /Q
 @git clone https://github.com/codewitch-honey-crisis/htcw_data
-@rmdir htcw_io /S /Q
-@git clone https://github.com/codewitch-honey-crisis/htcw_io
 @rmdir htcw_ml /S /Q
 @git clone https://github.com/codewitch-honey-crisis/htcw_ml
-
+@.\htcw_ml\fetch_deps.cmd
